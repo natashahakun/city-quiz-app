67f47e430026b20fc375000ca6f8aa24148e8b89
natashalynne <hakun.natasha@gmail.com>
2015-10-15 17:53:21 -0400

    Add js.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var currentQuestion = 0;
+
+var quiz = [
+  {
+    "question number":"bla bla bla?",
+    "options": [
+      "Option 0",
+      "Option 1",
+      "Option 2",
+      "Option 3"
+    ],
+    "correctAnswer": 2
+  }
+]
+
+var question = quiz[currentQuestion];
