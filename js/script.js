67013c89032ca553f737b87560e3a9a055e32633
natashalynne <hakun.natasha@gmail.com>
2015-10-18 21:12:17 -0400

    Page doesn't refresh when submit button is clicked.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var currentQuestion = 0;
 
 var quiz = [
   {
-    "question":" ",
+    "questionName":" ",
     "options": [
       "Option 1",
       "Option 2",
@@ -14,9 +14,9 @@ var quiz = [
 ]
 
 var askQuestion = function(){
-  document.getElementById("question").innerHTML = quiz[0].question;
-    for (i = 0; i < quiz[0].options.length; i++) {
-      document.getElementsByClassName("option")[i].innerHTML=quiz[0].options[i];
+  document.getElementById("question").innerHTML = question.questionName;
+  for (i = 0; i < question.options.length; i++) {
+    document.getElementsByClassName("option")[i].innerHTML=question.options[i];
   }
 }
 
@@ -24,20 +24,23 @@ var answerQuiz = function(question){
 
 }
 
-quiz[0] = {question: "Which city has the most miles of bike lanes?", options: ["Portland", "San Diego", "Phoenix", "New York"], correctAnswer: 1}
+quiz[0] = {questionName: "Which city has the most miles of bike lanes?", options: ["Portland", "San Diego", "Phoenix", "New York"], correctAnswer: 1}
 
-quiz[1] = {question: "Which city has the largest urban park?", options: ["Boston", "New York", "Minneapolis", "Scottsdale"], correctAnswer: 3}
+quiz[1] = {questionName: "Which city has the largest urban park?", options: ["Boston", "New York", "Minneapolis", "Scottsdale"], correctAnswer: 3}
 
-quiz[2] = {question: "Which city has the most eateries per capita?", options: ["New York", "Seattle", "San Francisco", "Austin"], correctAnswer: 2}
+quiz[2] = {questionName: "Which city has the most eateries per capita?", options: ["New York", "Seattle", "San Francisco", "Austin"], correctAnswer: 2}
 
-quiz[3] = {question: "Which city has the most colleges and universities?", options: ["Philadelphia", "Boston", "Chicago", "New York"], correctAnswer: 1}
+quiz[3] = {questionName: "Which city has the most colleges and universities?", options: ["Philadelphia", "Boston", "Chicago", "New York"], correctAnswer: 1}
 
-quiz[4] = {question: "Which city has the most trees per capita?", options: ["Sacramento", "Raleigh", "Boulder", "Portland"], correctAnswer: 0}
+quiz[4] = {questionName: "Which city has the most trees per capita?", options: ["Sacramento", "Raleigh", "Boulder", "Portland"], correctAnswer: 0}
 
 var question = quiz[currentQuestion];
 
 window.onload = function(){
   askQuestion();
   var form = document.getElementById("submit");
-  form.addEventListener("click", answerQuiz(quiz[0]));
+  form.addEventListener("click", function(event) {
+    event.preventDefault();
+    answerQuiz(question);
+  })
 }
