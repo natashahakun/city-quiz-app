953711fcc693fcfd7d94a7290f267f477ac8ccb6
natashalynne <hakun.natasha@gmail.com>
2015-10-20 17:00:32 -0400

    Fix event listeners to get correct score.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var currentQuestion = 0;
+var tally = 0;
 
 var quiz = [
   {
@@ -13,21 +14,35 @@ var quiz = [
   }
 ]
 
-var askQuestion = function(){
+var askQuestion = function(question){
   document.getElementById("question").innerHTML = question.questionName;
   for (i = 0; i < question.options.length; i++) {
     document.getElementsByClassName("option")[i].innerHTML=question.options[i];
   }
+  var oldSubmit = document.getElementById("submit");
+  var newSubmit = submit.cloneNode(false);
+  var form = document.getElementsByTagName("form")[0];
+  form.replaceChild(newSubmit, oldSubmit);
+  newSubmit.addEventListener("click", function(event) {
+    event.preventDefault();
+    answerQuiz(question);
+  })
 }
 
-var tally = 0;
-
 var answerQuiz = function(question){
-  var answer = document.getElementsByTagName("input")[correctAnswer];
+  var answer = document.getElementsByTagName("input")[question.correctAnswer];
   if (answer.checked == true) {
+    console.log("Correct!")
     tally += 1;
+    document.getElementById("score").innerHTML = ("Current number correct: " + tally);
   }
-  currentQuestion += 1;
+  if (currentQuestion < 4) {
+    currentQuestion += 1;
+    question = quiz[currentQuestion];
+    askQuestion(question);
+  } else {
+      document.getElementById("score").innerHTML = ("Your score is " + tally + " out of 5!");
+  }
 }
 
 quiz[0] = {questionName: "Which city has the most miles of bike lanes?", options: ["Portland", "San Diego", "Phoenix", "New York"], correctAnswer: 1}
@@ -43,10 +58,5 @@ quiz[4] = {questionName: "Which city has the most trees per capita?", options: [
 var question = quiz[currentQuestion];
 
 window.onload = function(){
-  askQuestion();
-  var form = document.getElementById("submit");
-  form.addEventListener("click", function(event) {
-    event.preventDefault();
-    answerQuiz(question);
-  })
+  askQuestion(question);
 }
